378f2b6f3d8e06795cbae954cf11b3ebef1fc42e
samarth-gandhii <samthirdacc@gmail.com>
2025-09-17 00:46:52 +0530

    to implement dp knapsack logic in optimizer

--- optimizer.cpp
+++ optimizer.cpp
@@ -1,12 +1,34 @@
 #include "optimizer.h"
-#include<vector>
-#include<string>
+#include <vector>
+#include <string>
 #include <sstream>
 #include <iostream>
-#include <filesystem> 
-#include <fstream>   
-//#include "huffman.h"
+#include <filesystem>
+#include <fstream>
+// #include "huffman.h"
 
 namespace fs = std::filesystem;
 
+void optimizer::optimizeFiles(std::vector<FileInfo> &files)
+{
+    for (auto &file : files)
+    {
+        if (shouldDelete(file))
+        {
 
+            std::cout << "Do you want to delete " << file.path << "? (y/n)";
+            char choice;
+            std::cin >> choice;
+            if (choice == 'y' || choice == 'Y')
+                deleteFile(file);
+        }
+        else if (shouldCompress(file))
+        {
+            std::cout << "Do you want to compress " << file.path << "? (y/n): ";
+            char choice;
+            std::cin >> choice;
+            if (choice == 'y' || choice == 'Y')
+                compressFile(file);
+        }
+    }
+}
