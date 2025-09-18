720f42a4f913535507721aaf0b29e8c540c3514e
Tanish-SuperRonin <81tanishshah@gmail.com>
2025-09-18 20:47:39 +0530

    Update summary.cpp

--- summary.cpp
+++ summary.cpp
@@ -7,18 +7,18 @@ void Summary::printStep(const std::string& stepName,
                         const ScanResult& after,
                         int filesAffected) {
     std::cout << "\n[" << stepName << "]\n";
-    std::cout << "Used Before : " << std::fixed << std::setprecision(2) << before.usedSpaceMB << " MB\n";
-    std::cout << "Used After  : " << std::fixed << std::setprecision(2) << after.usedSpaceMB << " MB\n";
-    std::cout << "Files Processed: " << filesAffected << "\n";
-    std::cout << "-----------------------------------\n";
+    std::cout << "Used Before: " << std::fixed << std::setprecision(2) << before.usedSpace / (1024.0 * 1024.0) << " MB" << std::endl;
+    std::cout << "Used After: " << std::fixed << std::setprecision(2) << after.usedSpace / (1024.0 * 1024.0) << " MB" << std::endl;
+    std::cout << "Files Processed: " << filesAffected << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
 }
 
 void Summary::printFinal(const ScanResult& start,
                          const ScanResult& end,
                          int totalFilesProcessed) {
     std::cout << "\n====== FINAL SUMMARY ======\n";
-    std::cout << "Initial Used : " << std::fixed << std::setprecision(2) << start.usedSpaceMB << " MB\n";
-    std::cout << "Final Used   : " << end.usedSpaceMB << " MB\n";
+    std::cout << "Initial Used : " << std::fixed << std::setprecision(2) << start.usedSpace / (1024.0 * 1024.0) << " MB\n";
+    std::cout << "Final Used   : " << std::fixed << std::setprecision(2) << end.usedSpace / (1024.0 * 1024.0) << " MB\n";
     std::cout << "Total Files Processed: " << totalFilesProcessed << "\n";
     std::cout << "===========================\n";
 }
